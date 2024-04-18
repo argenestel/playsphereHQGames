c34f32407a64a2fe4d9c7f2229f0b6e4b9ff13d3
argenestel <arpitpali68@gmail.com>
2024-04-19 02:43:26 +0530

    Woodcutter game

---
+++ woodcutterPHQ/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-woodcutter-1.0";
+const contentToCache = [
+    "Build/woodcutterPHQ.loader.js",
+    "Build/woodcutterPHQ.framework.js.br",
+    "Build/woodcutterPHQ.data.br",
+    "Build/woodcutterPHQ.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
