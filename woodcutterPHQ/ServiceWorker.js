972181e21d041708cadf2c6e9baad2f7fe58538c
argenestel <arpitpali68@gmail.com>
2024-04-19 19:36:22 +0530

    c

--- woodcutterPHQ/ServiceWorker.js
+++ woodcutterPHQ/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-woodcutter-1.0";
 const contentToCache = [
     "Build/woodcutterPHQ.loader.js",
-    "Build/woodcutterPHQ.framework.js.gz",
-    "Build/woodcutterPHQ.data.gz",
-    "Build/woodcutterPHQ.wasm.gz",
+    "Build/woodcutterPHQ.framework.js.br",
+    "Build/woodcutterPHQ.data.br",
+    "Build/woodcutterPHQ.wasm.br",
     "TemplateData/style.css"
 
 ];
